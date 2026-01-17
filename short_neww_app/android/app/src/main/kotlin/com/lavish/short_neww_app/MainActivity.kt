96224a540fdf8a909d35600a77371de4589a780c
MEKALA MATHEWS <lavishstarsoft@gmail.com>
2026-01-17 10:54:35 +0530

    Delete short_neww_app directory

--- short_neww_app/android/app/src/main/kotlin/com/lavish/short_neww_app/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.short_neww_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-    // Multidex is enabled in build.gradle.kts
-    // No additional code needed for modern Android
-}
